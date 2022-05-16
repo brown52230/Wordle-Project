c0eabe3c1a483b0a8cf8f25767c8b72776a97009
brown52230 <brown52230@mvctc>
2022-05-16 09:56:41 -0400

    python port

---
+++ wordle.py
@@ -0,0 +1,64 @@
+pref = "\033["
+reset = f"{pref}0m"
+
+class colors:
+    black = "30m"
+    red = "31m"
+    green = "32m"
+    yellow = "33m"
+    blue = "34m"
+    magenta = "35m"
+    cyan = "36m"
+    white = "37m"
+
+def guessLetter():
+    userGuess = ""
+    try:
+        userGuess = input(">> ")
+    except:
+        print("failed")
+    return userGuess
+
+def board(guess, turnsLeft, guesses):
+    temp = ""
+    
+    for i in guesses:
+        if (i != ""):
+            temp = i
+            for i, j in zip(temp, word):
+                found = False
+                if (i == j) :
+                    print("[{}] ".format(i), end = '');
+                    found = True
+                    continue
+
+                for k in temp:
+                    if (k == j): 
+                        print("({})".format(i), end = '')
+                        found = True
+                        break
+                if (found == False):
+                    print(".{}. ".format(i), end = '',)
+            print("")
+    for i in range(turnsLeft):  
+        print("- - - - -")
+
+
+global word
+word = "bread"
+
+solved = False
+guess = ""
+turnsLeft = 6
+turnCounter = 1
+guesses = list()
+while solved != True and turnsLeft > 0:
+    board(guess, turnsLeft, guesses)
+    guess = guessLetter()
+    print(guess)
+    turnsLeft -= 1
+    guesses.append(guess)
+    turnCounter += 1
+    if (guess == word): solved = True
+exit()
+
